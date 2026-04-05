84da10b65a98739cf4751c69cd5dc4e5fce9d22e
movveplaneta <ltrolandy@gmail.com>
2026-04-05 15:43:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -271,9 +271,9 @@ function animateValue(el, start, end, duration, suffix = '') {
     }, stepTime);
 }
 
-animateValue(balanceEl, 0, 42000000, 1500, '$');
-animateValue(profitEl, 0, 124, 1500, '%');
-animateValue(usersEl, 0, 20000, 1500);
+animateValue(balanceEl, 0, 42000000, 2000, '$');
+animateValue(profitEl, 0, 124, 2000, '%');
+animateValue(usersEl, 0, 20000, 2000);
 
 // ================= MINI CHARTS DINÁMICOS =================
 document.querySelectorAll('.mini-chart').forEach((canvas) => {
